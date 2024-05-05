ee4b0bf1dba1034338bc7f428e97c6b5f88cc5ac
Timy Villarmia <timyvillarmia@gmail.com>
2024-05-05 17:50:57 +0800

    event model

---
+++ Eventify/Data/Events.cs
@@ -0,0 +1,29 @@
+﻿using Eventify.DataAnnotations;
+using System.ComponentModel.DataAnnotations;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace Eventify.Data
+{
+    public class Events
+    {
+        public int Id { get; set; }
+
+        [Required]
+        public string? EventName { get; set; }
+
+        [Required]
+        public string? Status { get; set; }
+
+        [DataType(DataType.Date), Required, CustomDateAttribute]
+        public DateTime Date { get; set; }
+
+        [DataType(DataType.Time) , Required]
+        public TimeOnly StartTime { get; set; }
+
+        [DataType(DataType.Time ), Required]
+        public TimeOnly EndTime { get; set; }
+
+        [Required]
+        public string? Venue { get; set; }
+    }
+}
