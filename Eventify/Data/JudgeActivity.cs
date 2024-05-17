99bee6c08ab14603b94254d15432bc22a6f8632a
lance heitkamp <captainyabugger@gmail.com>
2024-05-17 16:37:53 +0800

    ParticipantsScore, Result, JudgeActivity, Migrated to DB and updated to DB

---
+++ Eventify/Data/JudgeActivity.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Eventify.Data
+{
+    public class JudgeActivity
+    {
+        [Key]
+        public int ID { get; set; }
+
+        [ForeignKey("User")]
+        public int UserId { get; set; }
+        public ApplicationUser User { get; set; }
+
+        [ForeignKey("Activity")]
+        public int ActivityId { get; set; }
+        public Activity Activity { get; set; }
+    }
+}
---
+++ Eventify/Data/ParticipantsScore.cs
@@ -0,0 +1,26 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Eventify.Data
+{
+    public class ParticipantsScore
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        public double Score { get; set; }
+
+        [ForeignKey("Criteria")]
+        public int CriteriaId { get; set; }
+        public Criteria Criteria { get; set; }
+
+        [ForeignKey("Participants")]
+        public int ParticipantId { get; set; }
+        public Participants Participant { get; set; }
+
+        [ForeignKey("Judge")]
+        public int JudgedBy { get; set; }
+        public JudgeActivity Judge { get; set; }
+    }
+}
