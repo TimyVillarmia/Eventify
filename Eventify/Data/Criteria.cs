ebd714332706759b752f8015aec9b4b5fc808141
justchelo <daguplochelo02@gmail.com>
2024-05-17 09:22:08 +0800

    feature/CriteriaAndParticipantsMods

---
+++ Eventify/Data/Criteria.cs
@@ -0,0 +1,48 @@
+﻿using Eventify.DataAnnotations;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Eventify.Data
+{
+    public class Criteria
+    {
+
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        public string CriteriaName { get; set; }
+
+        [ForeignKey("Activity")]
+        public int ActivityID { get; set; }
+
+        [Required]
+        public double Weight { get; set; }
+
+        [Required]
+        public double MaxScore { get; set; }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+}
