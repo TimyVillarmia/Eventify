45646b26d32e83bb45db51210ea570f1264b249b
philip <philipjohn.segarra@gmail.com>
2024-05-18 15:52:32 +0800

    fourth

---
+++ Eventify/Data/Migrations/20240518091749_migars.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Eventify.Migrations
+{
+    /// <inheritdoc />
+    public partial class migars : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
