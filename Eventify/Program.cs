ca22186bd1a57b82da28d774a2dbdef0bd58685e
Timy <timyvillarmia@gmail.com>
2024-05-20 19:41:18 +0800

    Update Program.cs

--- Eventify/Program.cs
+++ Eventify/Program.cs
@@ -68,15 +68,11 @@ builder.Services.AddSingleton<IEmailSender<ApplicationUser>, EmailSender>();
 builder.Services.AddServerSideBlazor().AddCircuitOptions(options => { options.DetailedErrors = true; });
 
 var app = builder.Build();
+
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
+if (!app.Environment.IsDevelopment())
 {
-    
-    app.UseMigrationsEndPoint();
-}
-else
-{
-   app.UseExceptionHandler("/Error", createScopeForErrors: true);
+    app.UseExceptionHandler("/Error", createScopeForErrors: true);
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
