5b1fb3e9546e8a1d0733f9df68d84e986b762503
Timy <timyvillarmia@gmail.com>
2024-05-20 18:01:13 +0800

    Update Program.cs

--- Eventify/Program.cs
+++ Eventify/Program.cs
@@ -69,17 +69,15 @@ builder.Services.AddServerSideBlazor().AddCircuitOptions(options => { options.De
 
 var app = builder.Build();
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseMigrationsEndPoint();
-}
-else
+if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Error", createScopeForErrors: true);
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
+    
 }
 
+app.UseMigrationsEndPoint();
 app.UseHttpsRedirection();
     
 app.UseStaticFiles();
